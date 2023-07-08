88285823beab5f7c9a5f1743d95aaf57f71ba213
Alexander Orzechowski <alex@ozal.ski>
2023-07-08 15:46:56 -0400

    scene_graph: Introduce sway_scene_descriptor
    Across a wayland compositor, there are multiple shells: It can be
    a toplevel, or a layer_shell, or even something more meta like a drag
    icon or highlight indicators when dragging windows around.
    
    This object lets us store values that represent these modes of operation
    and keep track of what object is being represented.

---
+++ include/sway/scene_descriptor.h
@@ -0,0 +1,18 @@
+#ifndef _SWAY_SCENE_DESCRIPTOR_H
+#define _SWAY_SCENE_DESCRIPTOR_H
+#include <wlr/types/wlr_scene.h>
+
+enum sway_scene_descriptor_type {
+	SWAY_SCENE_DESC_INVALID,
+};
+
+bool scene_descriptor_assign(struct wlr_scene_node *node,
+	enum sway_scene_descriptor_type type, void *data);
+
+void *scene_descriptor_try_get(struct wlr_scene_node *node,
+	enum sway_scene_descriptor_type type);
+
+void scene_descriptor_destroy(struct wlr_scene_node *node,
+	enum sway_scene_descriptor_type type);
+
+#endif
